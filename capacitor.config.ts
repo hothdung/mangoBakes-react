93c7a2da31246c39d1387ad6183e910e6cb81c71
Jutta Hassler <>
2021-07-30 00:17:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mango-bakes-cakes',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
